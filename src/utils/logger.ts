b87909ad2b7bac488c2f9c2449090e6790cbadf2
dsereboo <dsereboo@gmail.com>
2025-03-22 20:57:26 +0000

    editor controls

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,1 +1,44 @@
-//configure winston
+import * as winston from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+import { Context, MiddlewareFn } from 'telegraf'
+
+const fileTransport = new DailyRotateFile({
+    level: 'info',
+    datePattern: 'DD-MM-YY',
+    auditFile: 'logrotation.config.json',
+    dirname: process.env.LOGFOLDERPATH,
+    filename: 'telegram-chatbot-%DATE%',
+    extension: '.log',
+    utc: true,
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss' }),
+        winston.format.printf((info) => {
+            return `[${info.timestamp}] payload: ${JSON.stringify(info.message)}`
+        })
+    ),
+    handleExceptions: true
+})
+
+const consoleTransport = new winston.transports.Console({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY/MM/DD HH:mm:ss' }),
+        winston.format.printf((info) => {
+            return `[${info.timestamp}] payload: ${JSON.stringify(info.message)}`
+        })
+    )
+})
+
+const loggerConfig = {
+    level: 'info',
+    transports: [fileTransport, consoleTransport]
+}
+
+export const logger = winston.createLogger(loggerConfig)
+
+export const winstonLogger = (): MiddlewareFn<Context> => {
+    return (ctx, next) => {
+        logger.info(ctx.update)
+        next()
+    }
+}
